6dda7c87cc6345b1b1c25ccddac418c0b4861bae
zIthalo <ithalowillian@gmail.com>
2025-06-14 11:19:05 -0300

    app para limpar caracteres especiais
    O limpa.py foi criado com o intuito de auxiliar o usuário que trabalha com números de cadastros, possa copiar e colar no sistema removendo os separadores.

---
+++ apps/limpa.py
@@ -0,0 +1,29 @@
+import pyperclip
+import re
+import time
+
+
+def limpar_texto(texto):
+    # Reorganizando os caracteres para evitar erro de intervalo
+    caracteres_remover = r"[.,:/\s+*#-]"
+    return re.sub(caracteres_remover, "", texto)
+
+
+def monitorar_clipboard():
+    texto_anterior = ""
+    while True:
+        texto_atual = pyperclip.paste()  # Obtém o texto atual da área de transferência
+        if texto_atual != texto_anterior:  # Verifica se houve uma mudança
+            texto_limpo = limpar_texto(texto_atual)  # Limpa o texto
+            pyperclip.copy(texto_limpo)  # Substitui na área de transferência
+            print(f"Texto processado: {texto_limpo}")
+            texto_anterior = texto_atual
+
+        time.sleep(1)  # Aguarda um segundo antes de verificar novamente
+
+
+# Inicia o monitoramento
+monitorar_clipboard()
+
+# cpf 120.920.974-82
+# cpf 120,920,974 82

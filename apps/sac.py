1f7f1fbb20a77450763b88e97d67450dfb9d2162
zIthalo <ithalowillian@gmail.com>
2025-09-21 23:44:26 -0300

    updates
    melhorias no código e novas funcionalidades

--- apps/sac.py
+++ apps/sac.py
@@ -1,19 +1,62 @@
-import pyautogui
-import datetime
-import time
+from datetime import datetime
 import pyautogui
 import time
 import keyboard
 import tkinter as tk
-from datetime import datetime
 from tkinter import simpledialog
+import pyperclip
 
-time.sleep(5)
+#Msg Endereço não localizado
 
-# Obtém a hora atual
+respostas = [
+            #0
+            "Favor verificar.\n\n",
+            #1
+            "\nFomos na entrega da NF em assunto e o endereço não foi localizado.",
+            #2
+            "\nGentileza confirmar o endereço e caso o mesmo estiver divergente favor autorizar o custo de reentrega de 50% do valor do CT-e origem, nos encaminhar uma CC-e para endereço correto e informar um contato válido do cliente para alinhar nova tentativa de entrega.",
+            #3
+            "\n\nAguardamos um breve retorno a fim de evitar impactos no prazo de entrega previsto para o cliente.\n", 
+            #4
+            "\nFomos na entrega da NF em assunto e o destinatário estava ausente.", 
+            #5
+            "\nPara seguir com uma nova tentativa de entrega, precisamos da autorização para custo de reentrega (50% do vlor do CT-e origem) e de um contato válido do cliente para alinharmos.", 
+            #6
+            "\nCaso o endereço esteja divergente favor nos encaminhar uma CC-e para o local correto também.",
+            #7
+            "\nFavor verificar e auxiliar.",
+            #8
+            "\nEstamos na entrega da NF em assunto aguardando descarregar.",
+            #9
+            "\nChegada: ",
+            #10
+            "\nSaída: "
+]
+
+respostasWpp = [
+    #0
+    "*Ithalo Willian.*\n",
+    #1
+    "\nTudo bem por aqui. E você?", 
+    #2
+    "\nVou verificar agora. Um momento.",
+    #3
+    "\nTudo bem?",
+    #4
+    "\nPreciso de um auxílio com esta NF:"
+]
+
+diversos = [
+    #0 e-mail
+    "sac.blu@arletetransportes.com.br",
+    #1
+    "\nPrecisamos de um breve retorno sobre este caso para finalizarmos esta tratativa."
+]
+
+
+# Corrigindo a lógica de hora
 hora_atual = datetime.now().hour
 
-# Define a saudação com base na hora
 if 0 <= hora_atual < 12:
     saudacao = "Bom dia,"
 elif 12 <= hora_atual < 18:
@@ -21,58 +64,163 @@ elif 12 <= hora_atual < 18:
 else:
     saudacao = "Boa noite,"
 
-def endNaoLocalizado():
-    pyautogui.write(saudacao)
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    pyautogui.write("Favor verificar.")
-    time.sleep(2)
-    pyautogui.press("enter")
-    pyautogui.write("Fomos na entrega da NF em assunto e o endereço não foi localizado.")
-    time.sleep(2)
-    pyautogui.press("enter")
-    pyautogui.write("Gentileza confirmar o endereço e caso o mesmo estiver divergente favor autorizar o custo de reentrega de 50% do valor do CT-e origem, nos encaminhar uma CC-e para endereço correto e informar um contato válido do cliente para alinhar nova tentativa de entrega.")
-    time.sleep(3)
-    pyautogui.press("enter")
-    pyautogui.write("Aguardamos breve retorno, para não prejudicar a entrega no cliente.")
-    time.sleep(2.5)
+#Funções
 
+def endNaoLocalizado():
+        
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl","v")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    pyperclip.copy(respostas[0])
+    pyautogui.hotkey("ctrl","v")
+   
+    pyperclip.copy(respostas[1])
+    pyautogui.hotkey("ctrl","v")
+   
+    
+    pyperclip.copy(respostas[2])
+    pyautogui.hotkey("ctrl","v")
+        
+    pyperclip.copy(respostas[3])
+    pyautogui.hotkey("ctrl","v")
 
 def reentrega():
-    pyautogui.write(saudacao)
-    time.sleep(.5)
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    pyautogui.write("Favor verificar.")
-    time.sleep(.5)
-    pyautogui.press("enter")
-    pyautogui.write("Fomos na entrega da NF em assunto e o destinatário estava ausente.")
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.write("Para prosseguir com a entrega, precisamos da autorização para custo de nova tentativa com valor de 50% do valor do CT-e origem e de um contato válido do destinatário para alinharmos nova tentativa.") 
-    time.sleep(1.5)
-    pyautogui.press("enter")
-    pyautogui.write("Caso o endereço esteja divergente favor nos encaminhar uma CC-e para endereço correto também.")
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.write("Aguardamos breve retorno, para não prejudicar a entrega no cliente.")
 
-def abrir_caixa_de_dialogo():
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl","v")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    
+    pyperclip.copy(respostas[0])
+    pyautogui.hotkey("ctrl","v")
+    
+    pyperclip.copy(respostas[4])
+    pyautogui.hotkey("ctrl","v")
+   
+    pyautogui.press("enter")
+    pyperclip.copy(respostas[5])
+    pyautogui.hotkey("ctrl","v")
+   
+    pyautogui.press("enter")
+    pyperclip.copy(respostas[6])
+    pyautogui.hotkey("ctrl","v")
+    
+    pyperclip.copy(respostas[3])
+    pyautogui.hotkey("ctrl","v")
+
+def estadia():
+
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+
+    pyperclip.copy(respostas[7])
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+
+    pyperclip.copy(respostas[8])
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+
+    pyperclip.copy(respostas[9])
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+
+    pyperclip.copy(respostas[10])
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+
+def wpp():
+    pyperclip.copy(respostasWpp[0])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostasWpp[1])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostasWpp[2])
+    pyautogui.hotkey("ctrl", "v")
+
+def wpp1():
+    pyperclip.copy(respostasWpp[0])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostasWpp[2])
+    pyautogui.hotkey("ctrl", "v")
+    
+def wpp2():
+    pyperclip.copy(respostasWpp[0])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostasWpp[2])
+    pyautogui.hotkey("ctrl", "v")
+
+def wpp3():
+
+    pyperclip.copy(respostasWpp[0])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(respostasWpp[3])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostasWpp[4])
+    pyautogui.hotkey("ctrl", "v")
+
+def falarEmail():
+    pyperclip.copy(diversos[0])
+    pyautogui.hotkey("ctrl","v")
+
+def cobrar():
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    pyperclip.copy(diversos[1])
+    pyautogui.hotkey("ctrl", "v")
+
+def abrirCaixaDeDialogo():
     root = tk.Tk()
     root.withdraw()
     comando = simpledialog.askstring("Entrada", "Digite um comando:")
 
-    if comando in ["1", "r"]:
+    if comando in ["3", "r"]:
         reentrega()
-    elif comando in ["2", "nl"]:
+    elif comando in ["33", "nl"]:
         endNaoLocalizado()
+    elif comando in ["4", "retorno"]:
+        cobrar()
+    elif comando in ["44", "estadia"]:
+        estadia()
+    elif comando in ["1", "wpp"]:
+        wpp()
+    elif comando in ["11", "wpp1"]:
+        wpp1()
+    elif comando in ["2", "wpp3"]:
+        wpp3()
+    elif comando in ["22", "email"]:
+        falarEmail()
     else:
         print("Comando não reconhecido.")
-    
-# Adiciona o atalho para pressionar pressione Pause Break
-keyboard.add_hotkey("pause", abrir_caixa_de_dialogo)
+
+keyboard.add_hotkey("pause", abrirCaixaDeDialogo)
 
 print("Aguardando atalhos...")
-keyboard.wait()  # Aguarda a execução do programa
-
+keyboard.wait()

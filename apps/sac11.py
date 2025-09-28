a861590f20e156a9a7ea0fbce085220a0efa82dc
zIthalo <ithalowillian@gmail.com>
2025-09-27 23:36:58 -0300

    Update sac11.py
    melhoramentos e redução de código desnecessário

--- apps/sac11.py
+++ apps/sac11.py
@@ -39,57 +39,50 @@ def mostrar_comandos():
 #Msg Endereço não localizado
 
 respostas = [
-            #0
-            "Favor verificar.\n\n",
-            #1
-            "\nFomos na entrega da NF em assunto e o endereço não foi localizado.",
-            #2
-            "\nGentileza confirmar o endereço e caso o mesmo estiver divergente favor autorizar o custo de reentrega de 50% do valor do CT-e origem, nos encaminhar uma CC-e para endereço correto e informar um contato válido do cliente para alinhar nova tentativa de entrega.",
-            #3
-            "\n\nAguardamos um breve retorno a fim de evitar impactos no prazo de entrega previsto para o cliente.\n", 
-            #4
-            "\nFomos na entrega da NF em assunto e o destinatário estava ausente.", 
-            #5
-            "\nPara seguir com uma nova tentativa de entrega, precisamos da autorização para custo de reentrega (50% do vlor do CT-e origem) e de um contato válido do cliente para alinharmos.", 
-            #6
-            "\nCaso o endereço esteja divergente favor nos encaminhar uma CC-e para o local correto também.",
-            #7
-            "\nFavor verificar e auxiliar.",
-            #8
-            "\nEstamos na entrega da NF em assunto aguardando descarregar.",
-            #9
-            "\nChegada: ",
-            #10
-            "\nSaída: "
+            #0 Endereço não localizado
+            "Favor verificar.\n\nFomos na entrega da NF em assunto e o endereço não foi localizado.\nGentileza confirmar o endereço e caso o mesmo estiver divergente gentileza: \n1- Autorizar o custo de reentrega de 50% do valor do CT-e origem; \n2-Nos encaminhar uma CC-e para endereço correto;  \n3-Informar um contato válido do cliente para alinhar nova tentativa de entrega.\n\nAguardamos um breve retorno a fim de evitar impactos no prazo de entrega previsto para o cliente.\n",
+            #1 Reentrega    
+            "Favor verificar.\n\nFomos na entrega da NF em assunto e o destinatário estava ausente.\nPara seguir com uma nova tentativa de entrega, precisamos da autorização para custo de reentrega (50% do valor do CT-e origem) e de um contato válido do cliente para alinharmos.\nCaso o endereço esteja divergente favor nos encaminhar uma CC-e para o local correto também.\n\nAguardamos um breve retorno a fim de evitar impactos no prazo de entrega previsto para o cliente.\n",           
+            #2 Estadia
+            "\nFavor verificar e auxiliar.\nEstamos na entrega da NF em assunto aguardando descarregar.\nChegada: \nSaída: ",
+            #3 Mudou-se
+            "\nFomos na entrega da NF em assunto e nos informaram que o cliente mudou-se.\nGentileza verificar e nos encaminhar uma CC-e para o endereço correto e autorizar o custo de reentrega de 50% do valor do CT-e origem.\nAguardamos breve retorno afim de não prejudicar o prazo de entrega do cliente.",
+]
+
+agendamento = [
+            #0 informar agendamento
+            "A mercadoria da NF em assunto está agendada para dia  \nGentileza confirmar ciência.",
+            #1 informar agendamento risso
+            "A mercadoria da NF em assunto está agendada para dia:  \nPoderia me auxiliar alinhando junto a Risso?\n",
+            #2 Verificar retorno agendamento
+            "\nEsta mercadoria segue conforme agenda amanhã?"
+
 ]
 
 respostasWpp = [
     #0
-    "*Ithalo Willian:*\n\n",
+    "\n\nTudo bem por aqui. E você?\n\nVou verificar agora, só um momento.", 
     #1
-    "\n\nTudo bem por aqui. E você?", 
-    #2
     "\n\nVou verificar agora, só um momento.",
-    #3
+    #2
     "\n\nTudo bem?",
-    #4
+    #3
     "\n\nPreciso de um auxílio com esta NF: ",
-    #5
+    #4
     "\n\nSegue e-mail referente a NF: "
 ]
-
-mudouse = "\nFomos na entrega da NF em assunto e nos informaram que o cliente mudou-se.\nGentileza verificar e nos encaminhar uma CC-e para o endereço correto e autorizar o custo de reentrega de 50% do valor do CT-e origem.\nAguardamos breve retorno afim de não prejudicar o prazo de entrega do cliente."
-
 
 diversos = [
     #0 e-mail
     "sac.blu@arletetransportes.com.br",
     #1
-    "\n\nPrecisamos de um breve retorno sobre este caso para finalizarmos esta tratativa.",
+    "\nPrecisamos de um breve retorno sobre este caso para finalizarmos esta tratativa.",
     #2
     "\nEsta mercadoria está em rota de entrega hoje, deve ser finalizada em horário comercial até as 18h.",
     #3
-    "\n\nTemos retorno sobre este caso?"
+    "\nTemos retorno sobre este caso?",
+    #4
+    "\n\n\n"
 ]
 
 
@@ -98,119 +91,77 @@ diversos = [
 hora_atual = datetime.now().hour
 
 if 0 <= hora_atual < 12:
-    saudacao = "Bom dia."
+    saudacao = "Olá! Bom dia."
 elif 12 <= hora_atual < 18:
-    saudacao = "Boa tarde."
+    saudacao = "Olá! Boa tarde."
 else:
-    saudacao = "Boa noite."
+    saudacao = "Olá! Boa noite."
 
-#Funções
+#Funções INICIO***********************************************************
 
 def endNaoLocalizado():
         
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl","v")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+
     pyperclip.copy(respostas[0])
     pyautogui.hotkey("ctrl","v")
-   
-    pyperclip.copy(respostas[1])
-    pyautogui.hotkey("ctrl","v")
-   
-    
-    pyperclip.copy(respostas[2])
-    pyautogui.hotkey("ctrl","v")
-        
-    pyperclip.copy(respostas[3])
-    pyautogui.hotkey("ctrl","v")
-
+  
 def reentrega():
 
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl","v")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    
-    pyperclip.copy(respostas[0])
-    pyautogui.hotkey("ctrl","v")
-    
-    pyperclip.copy(respostas[4])
-    pyautogui.hotkey("ctrl","v")
-   
-    pyautogui.press("enter")
-    pyperclip.copy(respostas[5])
-    pyautogui.hotkey("ctrl","v")
-   
-    pyautogui.press("enter")
-    pyperclip.copy(respostas[6])
-    pyautogui.hotkey("ctrl","v")
-    
-    pyperclip.copy(respostas[3])
-    pyautogui.hotkey("ctrl","v")
 
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostas[1])
+    pyautogui.hotkey("ctrl","v")
+   
 def estadia():
 
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl", "v")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
 
-    pyperclip.copy(respostas[7])
+    pyperclip.copy(diversos[4])
     pyautogui.hotkey("ctrl", "v")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
 
-    pyperclip.copy(respostas[8])
+    pyperclip.copy(respostas[2])
     pyautogui.hotkey("ctrl", "v")
-    pyautogui.press("enter")
+  
+def saudar():
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
 
-    pyperclip.copy(respostas[9])
+    pyperclip.copy(respostasWpp[2])
     pyautogui.hotkey("ctrl", "v")
-    pyautogui.press("enter")
-
-    pyperclip.copy(respostas[10])
-    pyautogui.hotkey("ctrl", "v")
-    pyautogui.press("enter")
 
 def wpp():
-    pyperclip.copy(respostasWpp[0])
+    pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl", "v")
     
+    pyperclip.copy(respostasWpp[0])
+    pyautogui.hotkey("ctrl", "v")
+
+def wppVouVerificar():
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl", "v")
     
     pyperclip.copy(respostasWpp[1])
     pyautogui.hotkey("ctrl", "v")
-    
-    pyperclip.copy(respostasWpp[2])
-    pyautogui.hotkey("ctrl", "v")
-    
-def wppVouVerificar():
-    pyperclip.copy(respostasWpp[0])
-    pyautogui.hotkey("ctrl", "v")
-    
-    pyperclip.copy(saudacao)
-    pyautogui.hotkey("ctrl", "v")
-    
-    pyperclip.copy(respostasWpp[2])
-    pyautogui.hotkey("ctrl", "v")
 
 def wppAuxilioNf():
 
-    pyperclip.copy(respostasWpp[0])
-    pyautogui.hotkey("ctrl", "v")
-    
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl", "v")
 
-    pyperclip.copy(respostasWpp[3])
+    pyperclip.copy(respostasWpp[2])
     pyautogui.hotkey("ctrl", "v")
-    
-    pyperclip.copy(respostasWpp[4])
+
+    pyperclip.copy(respostasWpp[3])
     pyautogui.hotkey("ctrl", "v")
 
 def wppSegueEmail():
@@ -218,6 +169,7 @@ def wppSegueEmail():
     pyautogui.hotkey("ctrl","v")
     pyperclip.copy(respostasWpp[5])
     pyautogui.hotkey("ctrl","v")
+
 def wppMercEmRota():
     pyperclip.copy(diversos[2])
     pyautogui.hotkey("ctrl","v")
@@ -226,27 +178,67 @@ def falarEmail():
     pyperclip.copy(diversos[0])
     pyautogui.hotkey("ctrl","v")
     
-
 def cobrar():
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl", "v")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+
     pyperclip.copy(diversos[1])
     pyautogui.hotkey("ctrl", "v")
 
 def cobrar2():
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl","v")
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+
     pyperclip.copy(diversos[3])
     pyautogui.hotkey("ctrl","v")
 
 def mudouSe():
     pyperclip.copy(saudacao)
     pyautogui.hotkey("ctrl","v")
-    pyperclip.copy(mudouse)
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+    
+    pyperclip.copy(respostas[3])
     pyautogui.hotkey("ctrl","v")
+
+def agendar():
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(agendamento[0])
+    pyautogui.hotkey("ctrl", "v")
+
+def agendarRisso():
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(agendamento[1])
+    pyautogui.hotkey("ctrl", "v")
+
+def cobrarAgAm():
+    pyperclip.copy(saudacao)
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(diversos[4])
+    pyautogui.hotkey("ctrl", "v")
+
+    pyperclip.copy(agendamento[2])
+    pyautogui.hotkey("ctrl", "v")
+
+
 comando_digitado = ""
 
 def verificar_comando(e):
@@ -272,14 +264,21 @@ def verificar_comando(e):
             comando_digitado += tecla
 
         # Verifica comandos
-        if "//1" in comando_digitado:
+        if "//0" in comando_digitado:
+            apagar_comando(3)
+            saudar()
+            comando_digitado = ""
+
+        elif "//1" in comando_digitado:
             apagar_comando(3)
             wpp()
             comando_digitado = ""
+
         elif "//2" in comando_digitado:
             apagar_comando(3)
             wppVouVerificar()
             comando_digitado = ""
+
         elif "//3" in comando_digitado:
             apagar_comando(3)
             wppAuxilioNf()
@@ -288,8 +287,8 @@ def verificar_comando(e):
         elif "//4" in comando_digitado:
             apagar_comando(3)
             reentrega()
-
             comando_digitado = ""
+
         elif "//5" in comando_digitado:
             apagar_comando(3)
             endNaoLocalizado()
@@ -299,32 +298,58 @@ def verificar_comando(e):
             apagar_comando(3)
             mudouSe()
             comando_digitado = ""
+
         elif "//7" in comando_digitado:
             apagar_comando(3)
             cobrar()
             comando_digitado = ""
+
         elif "//8" in comando_digitado:
             apagar_comando(3)
             cobrar2()
             comando_digitado = ""
+
         elif "//9" in comando_digitado:
             apagar_comando(3)
             estadia()
             comando_digitado = ""
+
         elif "//e" in comando_digitado:
             apagar_comando(3)
             falarEmail()
             comando_digitado = ""
+
         elif "//r" in comando_digitado:
             apagar_comando(3)
             wppMercEmRota()
             comando_digitado = ""
+
         elif "//sg" in comando_digitado:
             apagar_comando(4)
             wppSegueEmail()
             comando_digitado = ""
+        
+        elif "//ag" in comando_digitado:
+            apagar_comando(4)
+            agendar()
+            comando_digitado = ""
+
+        elif "//ar" in comando_digitado:
+            apagar_comando(4)
+            agendarRisso()
+            comando_digitado = ""
+
+        elif "//ca" in comando_digitado:
+            apagar_comando(4)
+            cobrarAgAm()
+            comando_digitado = ""
+
         elif len(comando_digitado) > 10:
             comando_digitado = ""
+
+
+#Funções FIM ***********************************************************
+
 
 keyboard.on_press(verificar_comando)
 mostrar_comandos()
